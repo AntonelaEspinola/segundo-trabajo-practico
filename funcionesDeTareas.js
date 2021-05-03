dbd43d1b0745567732ae0d10e8806ba35f409285
Familia <antoespinola55@gmail.com>
2021-05-03 14:06:46 -0300

    Se termino el proyecto

---
+++ funcionesDeTareas.js
@@ -0,0 +1,23 @@
+let process = require("process");
+const fs = require("fs");
+const tareas = JSON.parse(fs.readFileSync("./tareas.json", "utf8"));
+
+let comandoLista = process.argv[2];
+
+let mostrarLista = (comando) => {
+if(comando ==null){
+    console.log('Atencion-Tienes que pasar una accion');
+}else
+  {
+      let accion;
+  switch (comando) {
+    case "listar": accion = tareas;
+      break;
+    default:
+      accion= 'No entiendo que quieres hacer';
+  }
+  return accion;
+}
+};
+console.log(mostrarLista(comandoLista));
+module.exports = mostrarLista;
